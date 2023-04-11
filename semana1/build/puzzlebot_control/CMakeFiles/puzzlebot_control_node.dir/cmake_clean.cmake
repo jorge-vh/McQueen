2233579509385b0fc5cd532f3d9a47b96f42524f
jorge-vh <jorgevh0310@gmail.com>
2023-04-11 13:50:10 -0600

    Other changes

---
+++ semana1/build/puzzlebot_control/CMakeFiles/puzzlebot_control_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/javh/Documents/McQueen/semana1/devel/lib/puzzlebot_control/puzzlebot_control_node"
+  "/home/javh/Documents/McQueen/semana1/devel/lib/puzzlebot_control/puzzlebot_control_node.pdb"
+  "CMakeFiles/puzzlebot_control_node.dir/src/puzzlebot_control_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/puzzlebot_control_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ semana1/build/puzzlebot_world/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/javh/Documents/McQueen/semana1/src/puzzlebot_world
+# Build directory: /home/javh/Documents/McQueen/semana1/build/puzzlebot_world
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
