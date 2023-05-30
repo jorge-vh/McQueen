c5775b6e4ccc577963b6d3cb2640ae4f3d21ad52
Fernando Cuellar <fercuellar2010@hotmail.com>
2023-05-30 10:56:41 -0600

    follow aruco mods

--- src/puzzlebot_sim/src/bugs/followAruco.py
+++ src/puzzlebot_sim/src/bugs/followAruco.py
@@ -5,22 +5,26 @@ import cv2
 from cv_bridge import CvBridge
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
+from std_msgs.msg import String
 import numpy as np
 
 # Constants for P control
-KP_angular = 0.003
+KP_angular = 0.0015
 
 # Constants for velocity limits
-MAX_ANGULAR_VELOCITY = 0.02  # Maximum angular velocity
+MAX_ANGULAR_VELOCITY = 0.015  # Maximum angular velocity
 LINEAR_VELOCITY = 0.05  # Constant linear velocity
 
 # Global variables
 current_marker = None
 bridge = CvBridge()
 cv_image = None
+target_aruco_id = 0  # Default target ArUco ID
+received_cerrado_message = False
+received_abierto_message = False
 
 class Marker:
-    def _init_(self):
+    def __init__(self):
         self.id = None
         self.x = None
         self.y = None
@@ -28,7 +32,7 @@ class Marker:
 
 # Callback function for image
 def image_callback(msg):
-    global cv_image
+    global cv_image, current_marker
 
     try:
         cv_image = bridge.imgmsg_to_cv2(msg, "bgr8")
@@ -50,7 +54,7 @@ def image_callback(msg):
 
     if ids is not None:
         # Find the marker with the largest size
-        largest_marker_index = np.argmax(ids)
+        largest_marker_index = np.argmax([cv2.contourArea(corner) for corner in corners])
         current_marker_id = ids[largest_marker_index][0]
         current_marker_corners = corners[largest_marker_index][0]
 
@@ -59,7 +63,6 @@ def image_callback(msg):
         marker_center_y = (current_marker_corners[0][1] + current_marker_corners[2][1]) / 2
 
         # Create a Marker object for the current marker
-        global current_marker
         current_marker = Marker()
         current_marker.id = current_marker_id
         current_marker.x = marker_center_x
@@ -81,15 +84,30 @@ def image_callback(msg):
     #cv2.imshow("Image", cv_image)
     #cv2.waitKey(1)
 
+# Callback function for gripperSTM messages
+def gripper_stm_callback(msg):
+    global received_cerrado_message, received_abierto_message
+
+    if msg.data == "cerrado":
+        received_cerrado_message = True
+    elif msg.data == "abierto":
+        received_abierto_message = True
+
 # Main control loop
-def control_loop():
+def control_loop(target_aruco_id):
     rospy.init_node('aruco_follower')
 
     # Subscribe to image topic
     rospy.Subscriber('/video_source/raw', Image, image_callback)
 
+    # Subscribe to gripperSTM topic
+    rospy.Subscriber('gripperSTM', String, gripper_stm_callback)
+
     # Publish velocity commands
     cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+    # Publish "gripperJetson" messages
+    gripper_jetson_pub = rospy.Publisher("gripperJetson", String, queue_size=10)
 
     # Create a Twist message for velocity commands
     twist_cmd = Twist()
@@ -98,7 +116,7 @@ def control_loop():
     rate = rospy.Rate(10)
 
     while not rospy.is_shutdown():
-        if current_marker is not None:
+        if current_marker is not None and current_marker.id == target_aruco_id:
             # Calculate the error between the current marker position and the center of the image
             image_height, image_width, _ = cv_image.shape
             image_center_x = image_width // 2
@@ -117,12 +135,41 @@ def control_loop():
             marker_area = current_marker.marker_size * current_marker.marker_size
 
             # If the marker area covers more than 10% of the image, stop the robot
-            if marker_area > 0.1 * image_width * image_height:
+            if marker_area > 0.08 * image_width * image_height and (current_marker.id == 0 or current_marker.id == 2 or current_marker.id == 1):
+                # Publish "agarrar" message
+                gripper_jetson_pub.publish("agarrar")
+
+                # Wait until "cerrado" message is received
+                while not received_cerrado_message:
+                    rate.sleep()
+
+                # Reset received_cerrado_message flag
+                received_cerrado_message = False
+
                 twist_cmd.linear.x = 0.0
                 twist_cmd.angular.z = 0.0
+                cmd_vel_pub.publish(twist_cmd)
+                return  # Exit the function to search for another ArUco
+
+            if marker_area > 0.17 * image_width * image_height and (current_marker.id == 10 or current_marker.id == 11 or current_marker.id == 12):
+                # Publish "soltar" message
+                gripper_jetson_pub.publish("soltar")
+
+                # Wait until "abierto" message is received
+                while not received_abierto_message:
+                    rate.sleep()
+
+                # Reset received_abierto_message flag
+                received_abierto_message = False
+
+                twist_cmd.linear.x = 0.0
+                twist_cmd.angular.z = 0.0
+                cmd_vel_pub.publish(twist_cmd)
+                return  # Exit the function to search for another ArUco
+
             else:
                 # If the marker is not centered, rotate with a proportional angular velocity
-                if abs(error_angular) > 20:
+                if abs(error_angular) > 40:
                     #twist_cmd.linear.x = 0.0
                     twist_cmd.angular.z = -angular_velocity
                 else:
@@ -133,16 +180,24 @@ def control_loop():
             # Publish the velocity command
             cmd_vel_pub.publish(twist_cmd)
 
+            # Print the ID of the current marker
+            print("Current marker ID:", current_marker.id)
+
         else:
-            # If no marker is detected, stop the robot
+            # If no marker is detected or the detected marker has a different ID, stop the robot
             twist_cmd.linear.x = 0.0
-            twist_cmd.angular.z = 0.3
+            twist_cmd.angular.z = 0.05
             cmd_vel_pub.publish(twist_cmd)
 
         rate.sleep()
 
-if __name__ == '_main_':
-    try:
-        control_loop()
-    except rospy.ROSInterruptException:
-        pass
+if __name__ == '__main__':
+    # Set the IDs of the target ArUcos
+    target_aruco_ids = [0, 11, 2, 12]
+
+    rospy.init_node('aruco_follower')
+
+    for target_aruco_id in target_aruco_ids:
+        print("Following ArUco with ID:", target_aruco_id)
+
+        control_loop(target_aruco_id)
